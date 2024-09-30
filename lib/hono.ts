241a378897c6525692c8c2cec9f17d15a2b51806
Sal Noto <me@salnoto.com>
2024-09-30 18:21:24 -0400

    Fixed Hono server auth by passing in headers

--- lib/hono.ts
+++ lib/hono.ts
@@ -1,4 +1,7 @@
 import { type AppType } from "@/app/api/[[...route]]/route";
 import { hc } from "hono/client";
+import { headers } from "next/headers";
 
-export const hono = hc<AppType>("http://localhost:3000/");
+export const hono = hc<AppType>(process.env.NEXT_PUBLIC_SITE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000", {
+    headers: Object.fromEntries(headers()),
+});
